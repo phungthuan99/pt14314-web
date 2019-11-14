21bddf9a3a2cebee19511e9577afbbc5ef47d000
thienth32 <thienth32@gmail.com>
2019-11-14 16:09:06 +0700

    using faker lib to generate dump data

--- dump-data.php
+++ dump-data.php
@@ -1,6 +1,15 @@
 <?php 
 require_once './commons/db.php';
 require_once './commons/constants.php';
+require_once './libs/Faker/autoload.php';
+
+$faker = Faker\Factory::create();
+
+// $name = $faker->name('female');
+// $company = $faker->company;
+// $now = $faker->date($format = 'Y-m-d', $max = 'now'); 
+// var_dump($name, $company, $now);die;
+
 // tạo dữ liệu mẫu cho bảng roles
 $roles = [
 	[
@@ -42,6 +51,14 @@ $users = [
 ];
 
 // tạo dữ liệu mẫu cho bảng categories
+for ($i=0; $i < 10; $i++) { 
+	$name = $faker->name('female');
+	$desc = $faker->realText($maxNbChars = 200, $indexSize = 2);
+	$showMenu = rand(0, 1);
+	$sql = "insert into categories (name, description, show_menu)
+			values ('$name', '$desc', $showMenu)";
+	executeQuery($sql);
+}
 // tạo dữ liệu mẫu cho bảng products
 // tạo dữ liệu mẫu cho bảng product_galleries
 // tạo dữ liệu mẫu cho bảng vouchers
